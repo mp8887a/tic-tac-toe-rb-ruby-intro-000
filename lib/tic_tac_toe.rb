66a7fc7fa7ec054914223839f8898bf37eab699e
Mike Pokalsky <mpoksky1989@gmail.com>
2021-03-24 17:42:43 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -57,27 +57,29 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip
-
+  index = input_to_index(user_input)
   if valid_move?(board, index)
     move(board, index, marker = "X")
 else
   turn(board)
   end
-  def won?(board)
-    WIN_COMBINATIONS.each do |win_combination|
-      win_index_1 = win_combination[0]
-      win_index_2 = win_combination[1]
-      win_index_3 = win_combination[2]
+end
 
-      position_1 = board[win_index_1]
-      position_2 = board[win_index_2]
-      position_3 = board[win_index_3]
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
 
-      if position_1 == "X" && position_2 == "X" && position_3 == "X"
-        return win_combination
-      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-        return win_combination
-      end
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combination
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combination      
+    end
     end
     false
   end
