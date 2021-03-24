a32f6f728eea7096f09f1a739399d693d3a4572d
Mike Pokalsky <mpoksky1989@gmail.com>
2021-03-24 17:50:50 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -89,9 +89,7 @@ def won?(board)
   def draw?(board)
     if !won?(board) && full?(board)
       return true
-    elsif !won?(board) && !full?(board)
-      return false
-    else won?(board)
+    else
       return false
     end
   end
