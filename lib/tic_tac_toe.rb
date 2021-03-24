d51f302558b935813861d28e4c728acc87afc620
Mike Pokalsky <mpoksky1989@gmail.com>
2021-03-24 18:01:28 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -57,7 +57,7 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index)
-    move(board, index, marker = "X")
+    move(board, index, current_player(board))
 else
   turn(board)
   end
